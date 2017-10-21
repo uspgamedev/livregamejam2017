513489e4c5d27d6eec5cd618f3273be082b85d97
IceMage144 <jgabriel.basi@hotmail.com>
2017-10-21 14:29:02 -0200

    Add a simple bfs power distributer for virus

--- game/graph_logic.lua
+++ game/graph_logic.lua
@@ -1,11 +1,41 @@
 
 local GRAPH_LOGIC = {}
 
-local _nodes = {}
-local _edges = {}
-local _infectedNodes = {}
 local _initialNode = 1 -- Test only variable
 local _testEdges = {{0, 5, 0}, {5, 0, 8}, {0, 8, 0}} -- Test only variable to simulate a file input
+local _power = 30
+local _lastInfected = false
+local _nodes = {}
+local _edges = {}
+
+local function compare(a, b)
+  return a.fin.pcs < b.fin.pcs
+end
+
+function bfsWeight(neighs)
+  local finish = true
+  local counter = _power
+  local notFullNodes = #neighs
+  local avg = _power/#neighs
+  while (counter ~= 0 and not finish) do
+    finish = true
+    avg = counter/notFullNodes
+    for i=1,#neighs do
+      local fin = _nodes[neighs[i].fin]
+      if not fin.infected and neighs[i].passing ~= 0 then
+        local add = math.min(fin.pcs-fin.infectedPcs, avg, counter, neighs[i].passing)
+        fin.infectedPcs = fin.infectedPcs + add
+        neighs[i].passing = neighs[i].passing - add
+        counter = counter - add
+        if fin.infectedPcs == fin.pcs then
+          fin.infected = true
+          notFullNodes = notFullNodes - 1
+        end
+        finish = false
+      end
+    end
+  end
+end
 
 function GRAPH_LOGIC.load(n)
   _nodes = {}
@@ -18,23 +48,30 @@ function GRAPH_LOGIC.load(n)
     _edges[i] = {}
     -- Add file infos to edges
     for j=1,n do
-      _edges[i][j] = (_testEdges[i] ~= 0) and ((j < i) and _edges[j][i] or { limit = 5 }) or false
+      _edges[i][j] = (_testEdges[i] ~= 0) and ((j < i) and _edges[j][i] or { weight = 5 }) or false
     end
   end
-  -- Add neighbors of initial node to linkedNodes
-  for node,edge in ipairs(_edges[_initialNode]) do
-    if edge then
-      table.insert(_infectedNodes, { id = node, rate = 0 })
-    end
-  end
-end
-
-function compare(a, b)
-  return a.pcs < b.pcs
+  -- Add initial node to _infectedNodes Nodes
 end
 
 function GRAPH_LOGIC.update(dt)
-  table.sort(_infectedNodes, compare)
+  local neighNodes = {}
+  for i=1,#_nodes do
+    for j=i+1,#_nodes do
+      if _edges[i][j] then
+        if not _nodes[i].infected and _nodes[j].infected then
+          table.insert(neighNodes, { ini = j, fin = i, passing = _edges[i][j].weight })
+        elseif _nodes[i].infected and not _nodes[j].infected then
+          table.insert(neighNodes, { ini = i, fin = j, passing = _edges[i][j].weight })
+        end
+      end
+    end
+  end
+  table.sort(neighNodes, compare)
+  bfsWeight(neighNodes)
 end
+
+GRAPH_LOGIC.nodes = _nodes
+GRAPH_LOGIC.edges = _edges
 
 return GRAPH_LOGIC
