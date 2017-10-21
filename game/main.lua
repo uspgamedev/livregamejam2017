133ccecfea6edb2881c51dc1171d6ad745362846
Kazuo256 <kazuo256@gmail.com>
2017-10-21 00:39:02 -0200

    Sample usage

--- game/main.lua
+++ game/main.lua
@@ -2,12 +2,16 @@
 local GRAPH_UI = require 'graph_ui'
 
 function love.load()
-  GRAPH_UI.load(1)
+  GRAPH_UI.load(3)
 end
 
 function love.update(dt)
   GRAPH_UI.update(dt)
-  GRAPH_UI.node(1, 400, 300)
+  if GRAPH_UI.node(1, 200, 300) then
+    print("node 1")
+  end
+  GRAPH_UI.node(2, 500, 400)
+  GRAPH_UI.node(3, 600, 100)
 end
 
 function love.draw()
